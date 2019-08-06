4d54d573839aec6ae5503dd7396d654ecb21a5be
Moon <Moon@192.168.0.130>
2019-08-07 00:36:58 +0900

    Update calenda main()

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,3 +1,6 @@
+/*
+ *  코드스쿼드 캘린더 만들기 v1 println 사용해서 출력하기.
+ */
 package honux.calendar;
 
 public class Calendar
@@ -6,7 +9,13 @@ public class Calendar
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hello, Calendar");
+		System.out.println(" 일   월    화    수    목    금    토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("29 30");
 	}
 
 }
