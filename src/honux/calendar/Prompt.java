8261421a3c5fc8a58af52a381f7661ad8d112662
Administrator <Administrator@DESKTOP-1JL2KJE>
2019-08-13 16:49:29 +0900

    UPDATE prompt UI changing

--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -5,43 +5,83 @@ import java.util.Scanner;
 public class Prompt
 {	
 
+	public void printMenu() {
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록");           
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말 q. 종료");
+		System.out.println("+----------------------+");
+		System.out.println("명령 (1, 2, 3, h, q)");		
+	}
+	
 	public void runPrompt()
 	{
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		int inputMonth = 0;
-		int inputYear = 0;
-		/* int weekDay = 0; */
-
-		while (true)
-		{
-			System.out.println("--연도를 입력하세요!!  *-1 입력시 종료*");
-			System.out.print("YEAR> ");
-			inputYear = scan.nextInt();
-			if (inputYear == -1)
-			{
+		
+		
+		while (true) {
+			System.out.println("명령 (1, 2, 3, h, q)");
+			String cmd = scan.next();
+			if(cmd.equals("1")) {
+				cmdRegister();
+			}
+			else if(cmd.equals("2")) {
+				cmdSearch();
+			}
+			else if(cmd.equals("3")) {
+				cmdCal(scan, cal);
+			}
+			else if(cmd.equals("h")) {
+				printMenu();
+			}
+			else if(cmd.equals("q")) {
 				break;
 			}
-			System.out.println("--달을 입력하세요!!  *-1 입력시 종료*");
-			System.out.print("MONTH> ");
-			inputMonth = scan.nextInt();
-			if (inputMonth == -1)
-			{
-				break;
-			} else if (inputMonth > 12 || inputMonth < 1)
-			{
-				System.out.println("1~12 사이의 값을 넣어주세요.");
-				continue;
-			}			
-
-			cal.printCalendar(inputYear, inputMonth);
-
 		}
-		;
+
+		
 		System.out.println("-------출력종료-------");
 
 		scan.close();
+	}
+
+	private void cmdCal(Scanner s, Calendar c)
+	{
+		int inputMonth = 0; // 달 입력
+		int inputYear = 0; // 연도 입력
+		// TODO Auto-generated method stub
+		
+			System.out.println("--연도를 입력하세요");
+			System.out.print("YEAR> ");
+			inputYear = s.nextInt();
+			
+			System.out.println("--달을 입력하세요.");
+			System.out.print("MONTH> ");
+			inputMonth = s.nextInt();			
+			if (inputMonth > 12 || inputMonth < 1)
+			{
+				System.out.println("1~12 사이의 값을 넣어주세요.");
+				return;
+			
+			}			
+			c.printCalendar(inputYear, inputMonth);
+
+		
+	}
+
+	private void cmdSearch()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void cmdRegister()
+	{
+		// TODO Auto-generated method stub
+		
 	}
 
 	public static void main(String[] args)
